2d87edf12d3d6b1abda388d601f8e5cf9dc39128
Thinh Dang <tiffythinhdang@gmail.com>
2019-09-08 14:34:15 -0700

    Setup npm

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './frontend/openkitchen.jsx',
+  output: {
+    filename: './bundle.js',
+    path: path.resolve(__dirname, "app", "assets", "javascripts")
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
