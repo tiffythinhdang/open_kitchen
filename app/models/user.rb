49c52b9af9a73c2f445f98b097d51b154faaf710
Thinh Dang <tiffythinhdang@gmail.com>
2019-09-08 20:20:14 -0700

    Create Session routes and controller. Add reset_session_token to User model. Add login/logout functionality to Application controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,6 +37,12 @@ class User < ApplicationRecord
   def ensure_session_token
     self.session_token ||= generate_session_token
   end
+
+  def reset_session_token! 
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
   
   def self.find_by_credentials(email, password)
     @user = User.find_by(email: email)
