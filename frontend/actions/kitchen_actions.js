429c0311f8ae473dce2ce9899fc3934845501918
Thinh Dang <tiffythinhdang@gmail.com>
2019-09-11 17:07:27 -0700

    Create kitchen actions

--- frontend/actions/kitchen_actions.js
+++ frontend/actions/kitchen_actions.js
@@ -1,3 +1,32 @@
-export const FETCH_KITCHENS = "FETCH_KITCHENS";
-export const FETCH_A_KITCHEN = "FETCH_A_KITCHEN";
+import * as KitchenAPIUtil from '../util/kitchens_api_util';
+
+export const RECEIVE_KITCHENS = "RECEIVE_KITCHENS";
+export const RECEIVE_A_KITCHEN = "RECEIVE_A_KITCHEN";
+
+//actions
+const receiveKitchens = (kitchens) => ({
+  type: RECEIVE_KITCHENS,
+  kitchens
+});
+
+const receiveAKitchen = (kitchen) => ({
+  type: RECEIVE_A_KITCHEN,
+  kitchen
+});
+
+//thunk actions
+export const fetchKitchens = () => dispatch => (
+  KitchenAPIUtil.fetchKitchens()
+    .then(kitchens => dispatch(receiveKitchens(kitchens)))
+  //Errors handling need to be done here! 
+);
+
+export const fetchAKitchen = (kitchenId) => dispatch => (
+  KitchenAPIUtil.fetchAKitchen(kitchenId)
+    .then(kitchen => dispatch(receiveAKitchen(kitchen)))
+  //Errors handling need to be done here! 
+);
+
+
+
 
