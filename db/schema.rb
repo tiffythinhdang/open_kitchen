b1cdb809026bfe3e7291dc8b3e55beb2eb8d6076
Thinh Dang <tiffythinhdang@gmail.com>
2019-09-14 22:40:38 -0700

    Remove redudant index on name in cities table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_13_193957) do
+ActiveRecord::Schema.define(version: 2019_09_15_053756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2019_09_13_193957) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name", "country"], name: "index_cities_on_name_and_country", unique: true
-    t.index ["name"], name: "index_cities_on_name"
   end
 
   create_table "cuisines", force: :cascade do |t|
