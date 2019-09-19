46d8a106de1c2966551abc9a5535fdf806a6275c
Thinh Dang <tiffythinhdang@gmail.com>
2019-09-19 11:27:57 -0700

    Add skeleton to saved_kitchen in the frontend. Need to comeback to to get the backend works

---
+++ frontend/components/dashboard/my_saved_kitchens.jsx
@@ -0,0 +1,26 @@
+// import React from 'react';
+
+// import DashBoardNav from './dashboard_nav';
+// import MySavedKitchenIndex from './my_saved_kitchen_index';
+
+// class MySavedKitchens extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   componentDidMount() {
+//     // this.props.fetchReservations(this.props.user.id);
+//   }
+
+//   render() {
+//     if (!this.props.user) return null
+//     return (
+//       <div className="dashboard container">
+//         <DashBoardNav />
+//         <MySavedKitchenIndex />
+//       </div>
+//     )
+//   }
+// }
+
+// export default MySavedKitchens;
