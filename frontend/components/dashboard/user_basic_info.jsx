6adf596c449685118e3abc0aa5198473e1561731
Thinh Dang <tiffythinhdang@gmail.com>
2019-10-22 22:51:16 -0700

    Readjust css for desktop view for user-basic-infor

--- frontend/components/dashboard/user_basic_info.jsx
+++ frontend/components/dashboard/user_basic_info.jsx
@@ -3,19 +3,6 @@ import React from 'react';
 const UserBasicInfo = ({ user }) => {
   return (
     <div className="user-basic-info container">
-      {/* <div className="user-basic-info titles-container">
-        <p className="user-title">First Name</p>
-        <p className="user-title">Last Name</p>
-        <p className="user-title">Email</p>
-        <p className="user-title">Location</p>
-      </div>
-      <div className="user-basic-info info-container">
-        <p>{user.firstName}</p>
-        <p>{user.lastName}</p>
-        <p>{user.email}</p>
-        <p>{user.location ? user.location : "Not provided"}</p>
-      </div> */}
-
       <div className="user-basic-info items-container">
         <div className="user-basic-info item name">
           <p className="user-title">First Name</p>
